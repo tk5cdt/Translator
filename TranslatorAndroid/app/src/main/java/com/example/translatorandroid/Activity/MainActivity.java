1d3c0d996e8d2dcf2e308ece835f133ae6f2a8d6
npbn42 <148618708+npbn42@users.noreply.github.com>
2024-08-05 19:51:31 +0700

    giao diện translate android

--- TranslatorAndroid/app/src/main/java/com/example/translatorandroid/Activity/MainActivity.java
+++ TranslatorAndroid/app/src/main/java/com/example/translatorandroid/Activity/MainActivity.java
@@ -1,26 +1,56 @@
 package com.example.translatorandroid.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.translatorandroid.Adapter.MainViewpageAdapter;
 import com.example.translatorandroid.R;
+import com.example.translatorandroid.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    MainViewpageAdapter adapter = new MainViewpageAdapter(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.vpMain.setAdapter(adapter);
+
+        binding.navBotMain.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.miTranslate) {
+                    binding.vpMain.setCurrentItem(0);
+                    return true;
+                } else {
+                    binding.vpMain.setCurrentItem(1);
+                    return true;
+                }
+            }
+        });
+
+        binding.vpMain.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0) {
+                    binding.navBotMain.setSelectedItemId(R.id.miTranslate);
+                } else if (position == 1) {
+                    binding.navBotMain.setSelectedItemId(R.id.miSaved);
+                }
+            }
         });
     }
 }
---
+++ TranslatorAndroid/app/src/main/java/com/example/translatorandroid/Adapter/MainViewpageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.translatorandroid.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.translatorandroid.Fragment.SavedFragment;
+import com.example.translatorandroid.Fragment.TranslateFragment;
+
+public class MainViewpageAdapter extends FragmentStateAdapter {
+
+    public  MainViewpageAdapter(@NonNull FragmentActivity fragmentActivity){
+        super(fragmentActivity);
+    }
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new TranslateFragment();
+            case 1:
+                return new SavedFragment();
+            default:
+                return new TranslateFragment();
+        }
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
